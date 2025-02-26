baef4557c7d9d6fe0f48964931ea41b2d2a67cc7
Mayke Alves <mayke.alves09@gmail.com>
2025-02-26 00:32:27 +0000

    Nv. Aventureiro concluido

---
+++ Aventureiro.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[10][10]; // Declaração de uma matriz 10x10
+
+    // Inicializando todos os elementos da matriz com 0 (água)
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            matriz[x][y] = 0;
+        }
+    }
+
+    // Posicionando o primeiro navio horizontalmente (linha 1, coluna 1)
+    matriz[1][1] = 3;
+    matriz[1][2] = 3;
+    matriz[1][3] = 3;
+
+    // Posicionando o segundo navio verticalmente (linha 5, coluna 5)
+    matriz[6][8] = 3;
+    matriz[7][8] = 3;
+    matriz[8][8] = 3;
+
+    // Posicionando o segundo navio verticalmente (linha 5, coluna 5)
+    matriz[1][6] = 3;
+    matriz[2][5] = 3;
+    matriz[3][4] = 3;
+
+    // Posicionando o segundo navio verticalmente (linha 5, coluna 5)
+    matriz[4][6] = 3;
+    matriz[5][5] = 3;
+    matriz[6][4] = 3;
+
+    // Exibindo o tabuleiro
+    printf("Tabuleiro do Batalha Naval:\n");
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            printf("%d ", matriz[x][y]); // Exibe cada elemento da matriz
+        }
+        printf("\n"); // Quebra de linha após cada linha da matriz
+    }
+
+    return 0;
+}
